232d96c2211622c0ec574bf40c30d5b173b3bf1e
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-09-21 14:14:45 +0300

    Refactor button layout and add reset functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let input = document.getElementById("input");
 let btn = document.getElementById("btn-barcode-generator");
+let resetBtn = document.getElementById("btn-reset"); // Reset düğmesini seç
 
 btn.addEventListener("click", () => {
   JsBarcode("#barcode", input.value, {
@@ -8,6 +9,11 @@ btn.addEventListener("click", () => {
     fontSize: 20,
     lineColor: "#000",
   });
+});
+
+resetBtn.addEventListener("click", () => {
+  input.value = ""; // Girdi alanını temizle
+  document.getElementById("barcode").innerHTML = ""; // Barcode SVG elementini temizle
 });
 
 window.onload = (event) => {
